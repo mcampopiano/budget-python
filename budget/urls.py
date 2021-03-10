701138e7a5b0bfcefff591fa472ff165798e8e8d
Mario Campopiano <mcampopiano92@gmail.com>
2021-03-09 22:53:00 -0600

    Wrote a view set for budgets and made a function for creating new ones. Succesfully tested in postman.

--- budget/urls.py
+++ budget/urls.py
@@ -1,8 +1,14 @@
 from django.conf.urls import include
 from django.urls import path
 from budgetapi.views import register_user, login_user
+from rest_framework import routers
+from budgetapi.views import Budgets
+
+router = routers.DefaultRouter(trailing_slash=False)
+router.register(r'budgets', Budgets, 'budget')
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('register', register_user),
     path('login', login_user),
     path('api-auth', include('rest_framework.urls', namespace='rest_framework')),
