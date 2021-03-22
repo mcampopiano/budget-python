7fa04002e678b6a42f8aa2fb8c6bbbf673f03066
Mario Campopiano <mcampopiano92@gmail.com>
2021-03-22 13:48:54 -0500

    reran migrations and reseeded the database

--- budgetapi/models/payment.py
+++ budgetapi/models/payment.py
@@ -1,7 +1,7 @@
-from django.db from django.db import models
+from django.db import models
 
 class Payment(models.Model):
-    recurring_bill = models.ForeignKey("RecurringBill")
-    budget = models.ForeignKey("Budget")
+    recurring_bill = models.ForeignKey("RecurringBill", on_delete=models.CASCADE)
+    budget = models.ForeignKey("Budget", on_delete=models.CASCADE)
     amount = models.FloatField()
     date_paid = models.DateField(auto_now=False, auto_now_add=False)
