d0300b6ffeec297e40c1c2eab861d441d8aee92b
Mario Campopiano <mcampopiano92@gmail.com>
2021-03-24 08:25:20 -0500

    created a method on the recurringBills viewset class for deleting payments

--- budgetapi/views/recurringBills.py
+++ budgetapi/views/recurringBills.py
@@ -33,6 +33,19 @@ class RecurringBills(ViewSet):
         serializer = RecurringBillSerializer(bill, context={'request': request})
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
+    def destroy(self, request, pk=None):
+        try:
+            payment = Payment.objects.get(pk=pk)
+            payment.delete()
+
+            return Response({}, status=status.HTTP_204_NO_CONTENT)
+
+        except Payment.DoesNotExist as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+
+        except Exception as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
     @action(methods=['POST', 'DELETE'], detail=True)
     def payments(self, request, pk=None):
         if (request.method == 'POST'):
